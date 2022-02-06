a0b1c669cce37c15c9fcedd391e05e5c71a6f168
khanshabebnaeem <khanshahebnaeem@gmail.com>
2022-02-06 11:21:52 +0600

    stateless widget

--- lib/git_test/test.dart
+++ lib/git_test/test.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Test extends StatefulWidget {
-  const Test({Key? key}) : super(key: key);
+class Trial extends StatelessWidget {
+  const Trial({Key? key}) : super(key: key);
 
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
     return Container();
